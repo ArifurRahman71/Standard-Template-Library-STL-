268d0f01201a25b8a67ac5ec9ccb6cba34ffaaf9
ArifurRahman71 <140314532+ArifurRahman71@users.noreply.github.com>
2023-09-21 23:13:00 -0700

    Add files via upload

---
+++ Vector/11.insert().cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	vector<int>v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+	v.push_back(5);
+
+for(int i=0;i<v.size();i++)
+	{
+		cout<<v[i]<< " ";
+	}
+	cout<<endl;
+
+// v.insert(v.begin(),1);
+
+// for(int i=0;i<v.size();i++)
+// 	{
+// 		cout<<v[i]<< " ";
+// 	}
+// 	cout<<endl;
+
+// 	v.insert(v.begin()+1,1);
+
+// for(int i=0;i<v.size();i++)
+// 	{
+// 		cout<<v[i]<< " ";
+// 	}
+// 	cout<<endl;
+	v.insert(v.begin()+2,3,1);
+
+for(int i=0;i<v.size();i++)
+	{
+		cout<<v[i]<< " ";
+	}
+	cout<<endl;
+
+
+
+	}

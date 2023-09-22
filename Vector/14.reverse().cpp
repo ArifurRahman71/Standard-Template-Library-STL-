3d8942f01b3baf33ccf0f027561742b018008adc
ArifurRahman71 <140314532+ArifurRahman71@users.noreply.github.com>
2023-09-21 23:01:52 -0700

    Update 14.reverse().cpp

--- Vector/14.reverse().cpp
+++ Vector/14.reverse().cpp
@@ -10,7 +10,7 @@ int main()
 	v1.push_back(5);
 	v1.push_back(10);
 
-	cout<< "before sroting"<<endl;
+	cout<< "before reverse"<<endl;
 	for(int i=0;i<v1.size();i++)
 	{
 		cout<<v1[i]<<" ";
@@ -19,7 +19,7 @@ int main()
 
 	reverse(v1.begin(),v1.end());
 
-	cout<< "After sroting"<<endl;
+	cout<< "After reverse"<<endl;
 	for(int i=0;i<v1.size();i++)
 	{
 		cout<<v1[i]<<" ";
